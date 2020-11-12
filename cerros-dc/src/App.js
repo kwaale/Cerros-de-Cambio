810a738eede540eb56decd2760bd323e2aca3053
Knut <kwaale@github.com>
2020-11-12 20:16:20 -0300

    Modelo 1 de Card

--- cerros-dc/src/App.js
+++ cerros-dc/src/App.js
@@ -4,12 +4,14 @@ import Persons from './Components/Persons.js';
 
 // dependencia que debes de agregar a la aplicacion react que generes https://storybook.js.org/
 const data = [{
-  "nombre": "Juan",
-  "apellido": "Topo",
+  "id":"001",
+  "nombre": "Juan Carlos",
+  "apellido": "Perez Gonzalez",
   "edad": 60,
   "telefono": "11-5555-1234"
 },
 {
+  "id":"002",
   "nombre": "Alice",
   "apellido": "Topa",
   "edad": 32,
--- cerros-dc/src/Components/Persons.js
+++ cerros-dc/src/Components/Persons.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import './Persons.css';
+import foto from '../img/Foto.jpg';
+import montania from '../img/Montania.png';
+
 
 //Persona renderizado
-export default function Persons({data}) {
+export default function Persons({ data }) {
     return (
         <div>
-            {data.map(e=>{
-                return(
-                <div className='card-persona'>
-                <h2>{e.nombre} {e.apellido}</h2>
-                <p>Edad: {e.edad}</p>
-                <p>Telefono: {e.telefono}</p>
-                </div>
+            {data.map(e => {
+                return (
+                    <div className='card-persona' key={e.id}>
+                        <div className='cuerpos'>
+                            <img className='montania' src={montania} alt='Montaña' />
+                            <h2 className='nombre-perfil' >{e.nombre} {e.apellido}</h2>
+                        </div>
+                        <div className='cuerpos'>
+                            <img className='foto-perfil' src={foto} alt='Foto Perfil' />
+                            <div className='data-persona'>
+                                <li>Edad: {e.edad}</li>
+                                <li>Telef: {e.telefono}</li>
+                                <li>Relleno: Relleno largo que puede ocupar varias lineas "En gastronomía
+                                    se llama relleno a la sustancia o mezcla comestible usada para llenar
+                                    una cavidad en otro alimento. Se rellenan muchos alimentos, incluyendo
+                                    carnes, verduras y frutas.".</li>
+                                <li>Relleno: Relleno corto.</li>
+                                <li>Relleno</li>
+                                <li>Relleno</li>
+                                <li>Relleno</li>
+                                <li>Relleno</li>
+                            </div>
+                        </div>
+                    </div>
                 )
             })}
         </div>
